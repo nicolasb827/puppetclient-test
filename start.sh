e753bc202efee6a20ed49d3ef014520891f0b799
nicolasb827 <nicolas.belan@gmail.com>
2017-04-19 12:35:23 +0200

    second commit

---
+++ start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function initiate_instance {
+  echo "Starting node initiation..."
+
+  # Fire up regular Puppet master to generate
+  # certificates and folder structure.
+  # This shouldn't take more than five seconds.
+  echo "Starting Puppet to generate certificates..."
+  timeout 5 puppet agent  --no-daemonize
+
+  echo "Node initation completed..."
+}
+
+cat - > /etc/puppetlabs/puppet/puppet.conf <<EOF
+[main]
+certname = $(hostname)
+server = $(hostname)
+masterport = 8149
+environment = production
+runinterval = 1h
+basemodulepath = /etc/puppetlabs/code/modules
+EOF
+
+if [ -f /master.conf ]; then
+	cat /master.conf >> /etc/puppetlabs/puppet/puppet.conf
+fi
+
+for CONF in puppetdb.conf hiera.yaml; do
+	if [ -f /$CONF ]; then
+		cp /$CONF /etc/puppetlabs/puppet/
+	fi
+done
+
+if [ ! -d /etc/puppetlabs/code/environments/production ]; then
+	mkdir -p /etc/puppetlabs/code/environments/production
+	chown puppet:puppet /etc/puppetlabs/code/environments
+fi
+
+bash
