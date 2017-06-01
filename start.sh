366ee14ecf5dc435ecca14c69084eac1b688f985
Nicolas <nicolas@belan.org>
2017-06-01 12:37:00 +0200

    More verbose start.sh loop until TERM Cleanup SSL on startup More detailed exit code when terminating Automatic SSL retrieval Specify ENV as ARG Specify CERT as ARG

--- start.sh
+++ start.sh
@@ -1,40 +1,82 @@
 #!/bin/bash
 
-function initiate_instance {
-  echo "Starting node initiation..."
+MY_ENV=$1
+MY_CERT=$2
 
-  # Fire up regular Puppet master to generate
-  # certificates and folder structure.
-  # This shouldn't take more than five seconds.
-  echo "Starting Puppet to generate certificates..."
-  timeout 5 puppet agent  --no-daemonize
+if [ "" == "$MY_ENV" ]; then
+	MY_ENV="production"
 
-  echo "Node initation completed..."
-}
+fi
 
+if [ "" == "$MY_CERT" ]; then
+	MY_CERT=$(hostname --fqdn)
+fi
+
+echo "[client] Starting using"
+echo "[client] - env : $MY_ENV"
+echo "[client] - cert: $MY_CERT"
+
+echo "[client] creating puppet.conf"
 cat - > /etc/puppetlabs/puppet/puppet.conf <<EOF
 [main]
-certname = $(hostname)
+certname = $MY_CERT
 server = puppetmaster
-masterport = 8149
-environment = production
+masterport = 8140
+environment = $MY_ENV
 runinterval = 1h
 basemodulepath = /etc/puppetlabs/code/modules
 EOF
 
 if [ -f /master.conf ]; then
+	echo "[client] appending (sys)master.conf to puppet.conf"
 	cat /master.conf >> /etc/puppetlabs/puppet/puppet.conf
 fi
 
 for CONF in puppetdb.conf hiera.yaml; do
 	if [ -f /$CONF ]; then
+		echo "[client] copy $CONF to /etc/puppetlabs/puppet/"
 		cp /$CONF /etc/puppetlabs/puppet/
 	fi
 done
 
-if [ ! -d /etc/puppetlabs/code/environments/production ]; then
-	mkdir -p /etc/puppetlabs/code/environments/production
-	chown puppet:puppet /etc/puppetlabs/code/environments
+if [ ! -d /etc/puppetlabs/code/environments/$MY_ENV ]; then
+	echo "[client] creating directory /etc/puppetlabs/code/environments/$MY_ENV"
+	mkdir -p /etc/puppetlabs/code/environments/$MY_ENV
+	chown -R puppet:puppet /etc/puppetlabs/code/environments
 fi
 
-bash
+echo "[client] creating helper script as /usr/local/bin/do_puppet.sh"
+cat - > /usr/local/bin/do_puppet.sh <<EOF
+#! /bin/bash
+
+puppet agent --test -w 10 \$@
+ret=\$?
+case \$ret in
+	0)
+		echo "The run succeeded with no changes or failures; the system was already in the desired state."
+		;;
+	1)
+		echo "The run failed, or wasn't attempted due to another run already in progress."
+		;;
+	2)
+		echo "The run succeeded, and some resources were changed."
+		;;
+	4)
+		echo "The run succeeded, and some resources failed."
+		;;
+	6)
+		echo "The run succeeded, and included both changes and failures."
+		;;
+esac
+exit \$ret
+EOF
+
+chmod a+x /usr/local/bin/do_puppet.sh
+
+echo "[client] cleaning any certificates"
+find /etc/puppetlabs/puppet/ssl/ -type f -exec rm -f {} \;
+
+echo "[client] ready !"
+while :; do
+  sleep 300
+done
